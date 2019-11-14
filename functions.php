a2bdefe9e2d637e5886eb1463faae3a62fa70cd1
Jim Ross <jimrossuk@gmail.com>
2019-11-14 11:28:08 +0000

    added CPT Case studies AND made Blog page to post functionality

--- functions.php
+++ functions.php
@@ -11,22 +11,25 @@ function filter_example($title) {
 	return 'Hooked: '.$title;
 }
 
+
+
 //adding a custom post types ---CPT
-function create_custom_post_type_retreat(){
+function create_custom_post_type_casestudy(){
 	$labels = array(
-        'name' => 'Retreats',
-        'singular_name' => 'Retreats',
-        'add_new' => 'Add New',
-        'add_new_item' => 'Add New Retreat',
-        'edit_item' => 'Edit Retreat',
-        'new_item' => 'New Retreat',
-        'view_item' => 'View Retreat',
-        'search_items' => 'Search Retreats',
-        'not_found' => 'No Retreats found in Trash',
-        'parent_item_colon' => '', 
-    );
+		'name' => 'Case Studies',
+		'singular_name' => 'Case Study',
+		'add_new' => 'Add New',
+		'add_new_item' => 'Add New Case',
+		'edit_item' => 'Edit Case',
+		'new_item' => 'New Case',
+		'view_item' => 'View Case',
+		'search_items' => 'Search Case Studies',
+		'not_found' => 'No events found',
+		'not_found_in_trash' => 'No events found in Trash',
+		'parent_item_colon' => '',
+	);
 	$args = array(
-		'label' => __('Events'),
+		'label' => __('Case Study'),
 		'labels' => $labels, // from array above
 		'public' => true,
 		'can_export' => true,
@@ -35,11 +38,18 @@ function create_custom_post_type_retreat(){
 		'capability_type' => 'post',
 		'menu_icon' => 'dashicons-calendar', // from this list
 		'hierarchical' => false,
-		'rewrite' => array( "slug" => "events" ), // defines URL base
+		'rewrite' => array( "slug" => "case_study" ), // defines URL base
 		'supports'=> array('title', 'thumbnail', 'editor', 'excerpt'),
 		'show_in_nav_menus' => true,
 		'taxonomies' => array( 'event_category', 'post_tag') // own categories
 	);
-	register_post_type('retreat', $args); // used as intern
+
+
+	register_post_type('case_study', $args); // used as internal identifier
 }
-add_action('init', 'create_custom_post_type_retreat' );
+
+add_action('init','create_custom_post_type_casestudy'); // define event custom post type
+
+
+
+
